b2026544ff718ce1ec42e245b714f7947433f839
Andrus Rinde <andrusrinde@users.noreply.github.com>
2023-10-05 13:29:35 +0300

    Viies tund lõppenud

--- tund5/www.js
+++ tund5/www.js
@@ -2,6 +2,7 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
+const { parse } = require('querystring');
 const dateInfo = require("./dateTime_et");
 const pageHead = '<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset="utf-8">\n\t<title>Andrus Rinde, veebiprogrammeerimine 2023</title>\n</head>\n<body>';
 const pageBanner = '\n\t<img src="banner.png" alt="Lehe bänner">\n';
@@ -9,96 +10,129 @@ const pageBody = '\n\t<h1>Andrus Rinde</h1>\n\t<p>See leht on loodud <a href="ht
 const pageFoot = '\n</body>\n</html>';
 
 http.createServer(function(req, res){
-	console.log(url.parse(req.url, true));
-	let currentURL = url.parse(req.url, true);
-	if(currentURL.pathname === "/"){
-		//määrame tagastatavate andmete päise, et on veebileht
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write(pageHead);
-		res.write(pageBanner);
-		res.write(pageBody);
-		res.write('\n\t <p>Lehe avamise hetkel oli:' + dateInfo.dateNowET() + ' kell ' + dateInfo.timeNowET() + '</p>');
-		res.write('\n\t <p><a href="addName">Lisame nime</a>!</p>');
-		res.write('\n\t <p>Semestri <a href="semesterprogress">edenemine</a>.</p>');
-		res.write('\n\t <p>TLÜ <a href="tluphoto">foto</a>.</p>');
-		res.write(pageFoot);
-		//et see kõik valmiks ja ära saadetaks
-		return res.end();
-	}
-	else if (currentURL.pathname === "/addName"){ 
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write(pageHead);
-		res.write(pageBanner);
-		res.write(pageBody);
-		res.write('<h2>Palun lisa oma nimi</h2>');
-		res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
-		res.write(pageFoot);
-		//et see kõik valmiks ja ära saadetaks
-		return res.end();
-	}
-	else if (currentURL.pathname === "/semesterprogress"){ 
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write(pageHead);
-		res.write(pageBanner);
-		res.write(pageBody);
-		res.write('\n\t<hr>');
-		res.write(semesterInfo());
-		res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
-		res.write(pageFoot);
-		//et see kõik valmiks ja ära saadetaks
-		return res.end();
-	}
-	
-	else if (currentURL.pathname === "/tluphoto"){ 
-		let htmlOutput = '\n\t<p>Pilti ei saa näidata!</p>';
-		//loen fotode nimekirja
-		fs.readdir('public/tluphotos', (err, fileList)=>{
-			if(err){
-				throw err;
-				tluPhotoPage(res, htmlOutput);
-			}
-			else {
-				//console.log(fileList.length);
-				let photoNum = Math.floor(Math.random() * fileList.length);
-				//console.log(photoNum);
-				//console.log('<img src="' + fileList[photoNum] + '" alt="TLÜ pilt">');
-				htmlOutput = '\n\t<img src="' + fileList[photoNum] + '" alt="TLÜ pilt">';
-				tluPhotoPage(res, htmlOutput);
-			}
-		});
-		
-	}
-	
-	else if (currentURL.pathname === "/banner.png"){
-		console.log("tahan pilti!");
-		let filePath = path.join(__dirname, "public", "banner/banner.png");
-		fs.readFile(filePath, (err, data)=>{
+	if (req.method === 'POST'){
+		//res.end('Tuligi POST!');
+		/* collectRequestData(req, result => {
+			res.write(result);
+			res.end();
+		}); */
+		//avan tekstifaili kirjutamiseks nii, et kui seda olemas pole, luuakse
+		fs.open('public/log.txt', 'a', (err, file)=>{
 			if(err){
 				throw err;
 			}
 			else {
-				res.writeHead(200, {"Content-Type": "image/png"});
-				res.end(data);
-			}
-		});
-	}
-	//else if (currentURL.pathname === "/tlu_42.jpg"){
-	else if (path.extname(currentURL.pathname) === '.jpg'){
-		console.log(path.extname(currentURL.pathname));
-		console.log("tahan jpg pilti!");
-		let filePath = path.join(__dirname, 'public', 'tluphotos');
-		fs.readFile(filePath + currentURL.pathname, (err, data)=>{
-			if(err){
-				throw err;
-			}
-			else {
-				res.writeHead(200, {"Content-Type": "image/jpeg"});
-				res.end(data);
+				fs.appendFile('public/log.txt', 'Tekst lisatud;', (err)=>{
+					if(err){
+						throw err;
+					}
+					else {
+						res.end('Lisati tekst');
+					}
+				});
+				fs.close(file, (err)=>{
+					if (err){
+						throw err;
+					}
+				});
 			}
 		});
 	}
 	else {
-		res.end('ERROR 404');
+		//console.log(url.parse(req.url, true));
+		let currentURL = url.parse(req.url, true);
+		if(currentURL.pathname === "/"){
+			//määrame tagastatavate andmete päise, et on veebileht
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write(pageHead);
+			res.write(pageBanner);
+			res.write(pageBody);
+			res.write('\n\t <p>Lehe avamise hetkel oli:' + dateInfo.dateNowET() + ' kell ' + dateInfo.timeNowET() + '</p>');
+			res.write('\n\t <p><a href="addName">Lisame nime</a>!</p>');
+			res.write('\n\t <p>Semestri <a href="semesterprogress">edenemine</a>.</p>');
+			res.write('\n\t <p>TLÜ <a href="tluphoto">foto</a>.</p>');
+			res.write(pageFoot);
+			//et see kõik valmiks ja ära saadetaks
+			return res.end();
+		}
+		else if (currentURL.pathname === "/addName"){ 
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write(pageHead);
+			res.write(pageBanner);
+			res.write(pageBody);
+			res.write('\n\t<h2>Palun lisa oma nimi</h2>');
+			res.write('\n\t<form method="POST">\n\t\t<label for="nameInput">Eesnimi: </label>\n\t\t<input type="text" id="nameInput" name="nameInput" placeholder="Sinu eesnimi ...">\n\t\t<br>\n\t\t<label for="lastNameInput">Perekonnanimi: </label>\n\t\t<input type="text" id="lastNameInput" name="lastNameInput" placeholder="Sinu perekonnanimi ...">\n\t\t<br>\n\t\t<input type="submit" name="nameSubmit" value="Salvesta">\n\t</form>');
+			res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
+			res.write(pageFoot);
+			//et see kõik valmiks ja ära saadetaks
+			return res.end();
+		}
+		else if (currentURL.pathname === "/semesterprogress"){ 
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write(pageHead);
+			res.write(pageBanner);
+			res.write(pageBody);
+			res.write('\n\t<hr>');
+			res.write(semesterInfo());
+			res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
+			res.write(pageFoot);
+			//et see kõik valmiks ja ära saadetaks
+			return res.end();
+		}
+		
+		else if (currentURL.pathname === "/tluphoto"){ 
+			let htmlOutput = '\n\t<p>Pilti ei saa näidata!</p>';
+			//loen fotode nimekirja
+			fs.readdir('public/tluphotos', (err, fileList)=>{
+				if(err){
+					throw err;
+					tluPhotoPage(res, htmlOutput);
+				}
+				else {
+					//console.log(fileList.length);
+					let photoNum = Math.floor(Math.random() * fileList.length);
+					//console.log(photoNum);
+					//console.log('<img src="' + fileList[photoNum] + '" alt="TLÜ pilt">');
+					htmlOutput = '\n\t<img src="' + fileList[photoNum] + '" alt="TLÜ pilt">';
+					tluPhotoPage(res, htmlOutput);
+				}
+			});
+			
+		}
+		
+		else if (currentURL.pathname === "/banner.png"){
+			console.log("tahan pilti!");
+			let filePath = path.join(__dirname, "public", "banner/banner.png");
+			fs.readFile(filePath, (err, data)=>{
+				if(err){
+					throw err;
+				}
+				else {
+					res.writeHead(200, {"Content-Type": "image/png"});
+					res.end(data);
+				}
+			});
+		}
+		//else if (currentURL.pathname === "/tlu_42.jpg"){
+		else if (path.extname(currentURL.pathname) === '.jpg'){
+			console.log(path.extname(currentURL.pathname));
+			console.log("tahan jpg pilti!");
+			let filePath = path.join(__dirname, 'public', 'tluphotos');
+			fs.readFile(filePath + currentURL.pathname, (err, data)=>{
+				if(err){
+					throw err;
+				}
+				else {
+					res.writeHead(200, {"Content-Type": "image/jpeg"});
+					res.end(data);
+				}
+			});
+		}
+
+		else {
+			res.end('ERROR 404');
+		}
+	
 	}
 }).listen(5200);
 
@@ -137,6 +171,24 @@ function tluPhotoPage(res, photoHTML){
 	res.write(pageFoot);
 	//et see kõik valmiks ja ära saadetaks
 	return res.end();
+}
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let receivedData = '';
+        request.on('data', chunk => {
+			console.log(chunk);
+            receivedData += chunk.toString();
+        });
+        request.on('end', () => {
+			console.log(receivedData);
+            callback(parse(receivedData));
+        });
+    }
+    else {
+        callback(null);
+    }
 }
 
 //5200   rinde
